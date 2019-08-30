f6d12ba6da78adb01eb9a72c7b00cafa86f75b7c
Victor Vilanova Bandeira <B39003@fgv.edu.br>
2019-08-30 17:06:30 -0300

    adcionando atributo rendimento domiciliar

--- pessoas.py
+++ pessoas.py
@@ -3,9 +3,31 @@ import numpy as np
 
 class pessoas:
     def __init__(self, n):
-        self.pessoa=[]
-    for i in range(n):
-        #  1 atributo: rendimento mensal domiciliar 5.1.1.1
+        self.tamanho=int(n)
+        self.individuo=self.pessoas(self.tamanho)
+
+    def pessoas(self, n):
+        lista = np.chararray((int(n), 16), itemsize=15)
+        for i in range(int(n)):
+
+            #  1 atributo: rendimento mensal domiciliar 5.1.1.1
+            if rm.random() <= np.random.normal(14908, 1.3)/157626:
+                lista[i,0]='até 1 S.M.'
+            elif rm.random() <= np.random.normal(45027, 0.8)/157626:
+                lista[i,0]='1 a 2 S.M.'
+            elif rm.random() <= np.random.normal(73020, 0.8)/157626:
+                lista[i,0]='2 a 3 S.M.'
+            elif rm.random() <= np.random.normal(109111, 0.7)/157626:
+                lista[i,0]='3 a 5 S.M."'
+            elif rm.random() <= np.random.normal(138220, 0.9)/157626:
+                lista[i,0]='5 a 10 S.M.'
+            elif rm.random() <= np.random.normal(147771, 1.9)/157626:
+                lista[i,0]='10 a 20 S.M.'
+            elif rm.random() <= np.random.normal(151181, 3.7)/157626:
+                lista[i,0]='Mais de 20 S.M.'
+            else:
+                lista[i,0]='Sem declaração'
+
 
         # 2 atributo: Vinculação ao PRONATEC 5.1.2.1
 
