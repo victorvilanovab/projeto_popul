a75775841ddfc12db70af3f8e0385613ff3780d6
Victor Vilanova Bandeira <B39003@fgv.edu.br>
2019-08-30 15:52:59 -0300

    Escolhendo os atributos

---
+++ pessoas.py
@@ -0,0 +1,38 @@
+import random as rm
+import numpy as np
+
+class pessoas:
+    def __init__(self, n):
+        self.pessoa=[]
+    for i in range(n):
+        #  1 atributo: rendimento mensal domiciliar 5.1.1.1
+
+        # 2 atributo: Vinculação ao PRONATEC 5.1.2.1
+
+        # 3 atributo: Tempo de duração de qualificação profissional 5.1.2.1
+
+        # 4 atributo: Modalidade 5.1.2.1
+
+        # 5 atributo: Turno 5.1.2.1
+
+        # 6 atributo: Dificuldade financeira 5.1.3.1
+
+        # 7 atributo: Dificuldade de acesso ao local do curso 5.1.3.1
+
+        # 8 atributo: Dificuldade de cumprir o horário do curso 5.1.3.1
+
+        # 9 atributo: Falta de tempo para estudar 5.1.3.1
+
+        # 10 atributo: Outras dificuldades para freguentar o curso de qualificação 5.1.3.1
+
+        # 11 atributo: Não havia dificuldades para frequentar o curso de qualificação 5.1.3.1
+
+        # 12 atributo: Sexo
+
+        # 13 atributo: Cor
+
+        # 14 atributo: Grupo de idade
+
+        # 15 atributo: Anos de estudo
+
+        # 16 atributo: Regiões
