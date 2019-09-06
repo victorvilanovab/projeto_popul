253b0e1288d73145f375b5dbab789ffe99d0a816
Victor Vilanova <victorvilanoova@gmail.com>
2019-09-05 21:26:18 -0300

    projeto finalizado

--- pessoas.py
+++ pessoas.py
@@ -8,8 +8,8 @@ class pessoas:
         self.individuo = self.pessoas (self.tamanho)
 
     def individuos(n):
-        lista_comprimento = np.arange(10 * n)
-        lista = lista_comprimento.reshape((n, 10))
+        lista_comprimento = np.arange(8 * n)
+        lista = lista_comprimento.reshape((n, 8))
         lista = np.array(lista, dtype='object')
 
         #0 atributo
@@ -20,24 +20,24 @@ class pessoas:
         #1 atributo
         hist1 = np.random.multinomial(int(n), [np.random.uniform(0.11,0.188), np.random.uniform(0.831,0.871)])
 
-        #4 atributo
-        hist4 = np.random.multinomial(int(n), [np.random.uniform(0.142, 0.180), np.random.uniform(0.122, 0.152),
+        #2 atributo
+        hist2 = np.random.multinomial(int(n), [np.random.uniform(0.142, 0.180), np.random.uniform(0.122, 0.152),
                                                  np.random.uniform(0.215, 0.243), np.random.uniform(0.216, 0.244),
                                                  np.random.uniform(0.219, 0.265)])
+        #3 atributo
+        hist3 = np.random.multinomial(int(n), [np.random.uniform(0.43, 0.48), np.random.uniform(0.524, 0.566)])
+
+        #4 atributo
+        hist4 = np.random.multinomial(int(n), [np.random.uniform(0.42, 0.48), np.random.uniform(0.514, 0.558)])
+
         #5 atributo
-        hist5 = np.random.multinomial(int(n), [np.random.uniform(0.43, 0.48), np.random.uniform(0.524, 0.566)])
+        hist5 = np.random.multinomial(int(n), [np.random.uniform(0.154, 0.228), np.random.uniform(0.789, 0.829)])
 
         #6 atributo
-        hist6 = np.random.multinomial(int(n), [np.random.uniform(0.42, 0.48), np.random.uniform(0.514, 0.558)])
+        hist6 = np.random.multinomial(int(n), [np.random.uniform(0.927, 0.963), np.random.uniform(0.023, 0.087)])
 
         #7 atributo
-        hist7 = np.random.multinomial(int(n), [np.random.uniform(0.154, 0.228), np.random.uniform(0.789, 0.829)])
-
-        #8 atributo
-        hist8 = np.random.multinomial(int(n), [np.random.uniform(0.927, 0.963), np.random.uniform(0.023, 0.087)])
-
-        #9 atributo
-        hist9 = np.random.multinomial(int(n), [np.random.uniform(0.199, 0.255), np.random.uniform(0.765, 0.781)])
+        hist7 = np.random.multinomial(int(n), [np.random.uniform(0.199, 0.255), np.random.uniform(0.765, 0.781)])
 
 
         for i in range (int (n)):
@@ -71,15 +71,7 @@ class pessoas:
                 lista[i, 0] = "Sem declaração"
                 hist0[escolha0] -= 1
 
-        # 2 atributo: Vinculação ao PRONATEC 5.1.2.1
-
-        # 3 atributo: Tempo de duração de qualificação profissional 5.1.2.1
-
-        # 5 atributo: Turno 5.1.2.1
-
-        # 6 atributo: Dificuldade financeira 5.1.3.1
-
-        # 1 atributo: Dificuldade de acesso ao local do curso 5.1.3.1
+        # 2 atributo: Dificuldade de acesso ao local do curso 5.1.3.1
             classes_c_valor1 = np.array(np.nonzero(hist1))
             c1 = classes_c_valor1.tolist ()
             d1 = c1[0]
@@ -93,108 +85,90 @@ class pessoas:
 
 
 
-        # 8 atributo: Modalidade
-            classes_c_valor8 = np.array(np.nonzero(hist8))
-            c8 = classes_c_valor8.tolist()
-            d8 = c8[0]
-            escolha8 = d8[random.randint(0, np.size(classes_c_valor8) - 1)]
-            if escolha8 == 0:
-                lista[i, 8] = 'Presencial'
-                hist8[escolha8] -= 1
+        #6 atributo: Modalidade
+            classes_c_valor6 = np.array(np.nonzero(hist6))
+            c6 = classes_c_valor6.tolist()
+            d6 = c6[0]
+            escolha6 = d6[random.randint(0, np.size(classes_c_valor6) - 1)]
+            if escolha6 == 0:
+                lista[i, 6] = 'Presencial'
+                hist6[escolha6] -= 1
             else:
-                lista[i, 8] = 'A distancia'
-                hist8[escolha8] -= 1
+                lista[i, 6] = 'A distancia'
+                hist6[escolha6] -= 1
 
-        # 9 atributo: Frquentaram ou nao o curso de qualificacao profissional
-            classes_c_valor9 = np.array(np.nonzero(hist9))
-            c9 = classes_c_valor9.tolist()
-            d9 = c9[0]
-            escolha9 = d9[random.randint(0, np.size(classes_c_valor9) - 1)]
-            if escolha9 == 0:
-                lista[i, 9] = "Frequentou"
-                hist9[escolha9] -= 1
-            else:
-                lista[i, 9] = "Não frequentou"
-                hist9[escolha9] -= 1
-
-
-        # 10 atributo: Outras dificuldades para freguentar o curso de qualificação 5.1.3.1
-
-        # 11 atributo: Não havia dificuldades para frequentar o curso de qualificação 5.1.3.1
-
-        # 5 atributo: Sexo
-            classes_c_valor5 = np.array(np.nonzero(hist5))
-            c5= classes_c_valor5.tolist()
-            d5 = c5[0]
-            escolha5 = d5[random.randint(0, np.size(classes_c_valor5) - 1)]
-            if escolha5 == 0:
-                lista[i, 5] = 'Homem'
-                hist5[escolha5] -= 1
-            else:
-                lista[i, 5] = 'Mulher'
-                hist5[escolha5] -= 1
-
-        # 7 atributo: Rede de ensino (Curso de qualificação profissional)
+        #7 atributo: Frquentaram ou nao o curso de qualificacao profissional
             classes_c_valor7 = np.array(np.nonzero(hist7))
             c7 = classes_c_valor7.tolist()
             d7 = c7[0]
             escolha7 = d7[random.randint(0, np.size(classes_c_valor7) - 1)]
             if escolha7 == 0:
-                lista[i, 7] = 'Pública'
+                lista[i, 7] = "Frequentou"
                 hist7[escolha7] -= 1
             else:
-                lista[i, 7] = 'Particular'
+                lista[i, 7] = "Não frequentou"
                 hist7[escolha7] -= 1
 
 
-
-        # # 13 atributo: Cor
-            classes_c_valor6 = np.array (np.nonzero (hist6))
-            c6 = classes_c_valor6.tolist ()
-            d6 = c6[0]
-            escolha6 = d6[random.randint (0, np.size (classes_c_valor6) - 1)]
-            if escolha6 == 0:
-                lista[i, 6] = 'Branco'
-                hist6[escolha6] -= 1
+        #3 atributo: Sexo
+            classes_c_valor3 = np.array(np.nonzero(hist3))
+            c3= classes_c_valor3.tolist()
+            d3 = c3[0]
+            escolha3 = d3[random.randint(0, np.size(classes_c_valor3) - 1)]
+            if escolha3 == 0:
+                lista[i, 3] = 'Homem'
+                hist3[escolha3] -= 1
             else:
-                lista[i, 6] = 'Preto ou pardo'
-                hist6[escolha6] -= 1
+                lista[i, 3] = 'Mulher'
+                hist3[escolha3] -= 1
 
-        # 14 atributo: Grupo de idade
+        # 7 atributo: Rede de ensino (Curso de qualificação profissional)
+            classes_c_valor5 = np.array(np.nonzero(hist5))
+            c5 = classes_c_valor5.tolist()
+            d5 = c5[0]
+            escolha5 = d5[random.randint(0, np.size(classes_c_valor5) - 1)]
+            if escolha5 == 0:
+                lista[i, 5] = 'Pública'
+                hist5[escolha5] -= 1
+            else:
+                lista[i, 5] = 'Particular'
+                hist5[escolha5] -= 1
 
-        # 15 atributo: Anos de estudo
 
-        # 4 atributo: Regiões
+
+        #4 atributo: Cor
             classes_c_valor4 = np.array (np.nonzero (hist4))
             c4 = classes_c_valor4.tolist ()
             d4 = c4[0]
             escolha4 = d4[random.randint (0, np.size (classes_c_valor4) - 1)]
-
-
             if escolha4 == 0:
-                lista[i, 4] = 'Norte'
-                hist4[escolha4] -= 1
-            elif escolha4 == 1:
-                lista[i, 4] = 'Nordeste'
-                hist4[escolha4] -= 1
-            elif escolha4 == 2:
-                lista[i, 4] = 'Sudeste'
-                hist4[escolha4] -= 1
-            elif escolha4 == 3:
-                lista[i, 4] = 'Sul'
+                lista[i, 4] = 'Branco'
                 hist4[escolha4] -= 1
             else:
-                lista[i, 4] = 'Centro-Oeste'
+                lista[i, 4] = 'Preto ou pardo'
                 hist4[escolha4] -= 1
-        #
-        print(lista)
-        print(hist0)
-        print(hist1)
-        print(hist4)
-        print(hist5)
-        print(hist6)
-        print(hist7)
-        print(hist8)
-        print(hist9)
 
-pessoas.individuos(10)
+        #2 atributo: Regiões
+            classes_c_valor2 = np.array (np.nonzero (hist2))
+            c2 = classes_c_valor2.tolist ()
+            d2 = c2[0]
+            escolha2 = d2[random.randint (0, np.size (classes_c_valor2) - 1)]
+            if escolha2 == 0:
+                lista[i, 2] = 'Norte'
+                hist2[escolha2] -= 1
+            elif escolha2 == 1:
+                lista[i, 2] = 'Nordeste'
+                hist2[escolha2] -= 1
+            elif escolha2 == 2:
+                lista[i, 2] = 'Sudeste'
+                hist2[escolha2] -= 1
+            elif escolha2 == 3:
+                lista[i, 2] = 'Sul'
+                hist2[escolha2] -= 1
+            else:
+                lista[i, 2] = 'Centro-Oeste'
+                hist2[escolha2] -= 1
+
+        return lista
+
+
